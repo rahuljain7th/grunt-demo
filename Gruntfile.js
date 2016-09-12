709744a18267c1ffdd1961b19bc9d4e5dd15e341
Rahul <jain_rahul@ymail.com>
2016-09-11 23:55:17 -0400

    Demonstration of concatenating javascript using grunt.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt) {
 grunt.loadNpmTasks('grunt-sass');
+grunt.loadNpmTasks('grunt-contrib-concat');
 grunt.initConfig({
    sass:{
         dist: {
@@ -7,8 +8,14 @@ grunt.initConfig({
             dest:'dist/css/style.css'
 
             }
-        }
+        },
+    concat : {
+         dist : {
+            src:'src/js/*.js',
+            dest:'dist/js/app.js'
+         }
+    }
 });
-grunt.registerTask('default',['sass']);
+grunt.registerTask('default',['sass','concat']);
 }
 
