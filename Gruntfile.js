61473939a3e422d9b523838280a81ab12a93d9bc
Rahul <jain_rahul@ymail.com>
2016-09-12 00:08:47 -0400

    Usage of Load-Grunt-Task plugin... to avode npm.loadgrunttask

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
-grunt.loadNpmTasks('grunt-sass');
-grunt.loadNpmTasks('grunt-contrib-concat');
+
+//looks into node_modeuls folder  to load grunt task which is needed
+require('load-grunt-tasks')(grunt);
+
+
 grunt.initConfig({
    sass:{
         dist: {
