a4b7fbd4b62686730ef1000664df4f441f468755
Rahul <jain_rahul@ymail.com>
2016-09-11 23:12:14 -0400

    compilation of sass to css using grunt
    Signed-off-by: Rahul <jain_rahul@ymail.com>

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+grunt.loadNpmTasks('grunt-sass');
+grunt.initConfig({
+   sass:{
+        dist: {
+            src:'src/sass/style.scss',
+            dest:'dist/css/style.css'
+
+            }
+        }
+});
+grunt.registerTask('default',['sass']);
+}
+
